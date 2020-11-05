d7e20cd044f4990167548ebace901f7feb04bb21
Nikita <nkrivorotov@bk.ru>
2020-11-06 03:47:15 +0700

    add file in directory test

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include "test.h"
+
+
+int main(int argc, char* argv[]) {
+	
+	Hello();
+	
+	return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include "test.h"
+
+void Hello() {
+	std::cout << "Hello world" << std::endl;
+}
