20ca0580a461965131ee697fda21a662e1142972
Nikita <nkrivorotov@bk.ru>
2020-11-07 00:12:00 +0700

    edit travis, add all test function in GtestCalc

--- test/GtestCalc.cpp
+++ test/GtestCalc.cpp
@@ -10,9 +10,27 @@ TEST(DefaultTestStep, StepTwo)
 TEST(DefaultTestPlus, PlusTest)
 {
 	EXPECT_EQ(7, Plus(2, 5));
-	EXPECT_EQ(10, Plus(7, 3);
+	EXPECT_EQ(10, Plus(7, 3));
 	EXPECT_EQ(1, Plus(-4, 5));
 	EXPECT_EQ(3, Plus(7, -4));
+}
+
+TEST(DefaultTestMinus, MinusTest)
+{
+	EXPECT_EQ(4, Minus(9, 5));
+	EXPECT_EQ(4, Minus(2, -2));
+}
+
+TEST(DefaultTestDel , DelTest)
+{
+	EXPECT_EQ(3, Del(9, 3));
+	EXPECT_EQ(-1, Del(4, -4));
+}
+
+TEST(DefaultTestUmn, UmnTest)
+{
+	EXPECT_EQ(9, Umn(3, 3));
+	EXPECT_EQ(-8, Umn(2, -4));
 }
 
 int main(int argc, char** argv) {
