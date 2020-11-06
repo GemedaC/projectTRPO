b5107b006eef9de445c57d345f7527cffa9719c9
Nikita <nkrivorotov@bk.ru>
2020-11-06 23:25:33 +0700

    edit travis, add test function Plus in GtestCalc

--- test/GtestCalc.cpp
+++ test/GtestCalc.cpp
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
 #include "..\src\funcCalc.cpp"
 
-TEST (DefaultTestStep, StepTwo)
+TEST(DefaultTestStep, StepTwo)
 {
-	EXPECT_EQ(4, Step(2,2));
+	EXPECT_EQ(4, Step(2, 2));
+	EXPECT_EQ(9, Step(3, 2));
+}
+
+TEST(DefaultTestPlus, PlusTest)
+{
+	EXPECT_EQ(7, Plus(2, 5));
+	EXPECT_EQ(10, Plus(7, 3);
+	EXPECT_EQ(1, Plus(-4, 5));
+	EXPECT_EQ(3, Plus(7, -4));
 }
 
 int main(int argc, char** argv) {
